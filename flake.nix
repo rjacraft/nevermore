7903647c71a6a3fef294607839c809aab74e1ac6
Petr Portnov | PROgrm_JARvis <me@progrm-jarvis.ru>
2025-06-14 12:09:37 +0300

    build(nix): configure environment (#117)
    * build(nix): configure flake
    
    * ci: pin actions' commits

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Nevermore";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [ inputs.git-hooks.flakeModule ];
+      perSystem =
+        {
+          system,
+          pkgs,
+          config,
+          ...
+        }:
+        let
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+          devShells.default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.cargo-msrv
+              pkgs.pinact
+            ];
+            shellHook = config.pre-commit.installationScript;
+          };
+          pre-commit.settings.hooks = {
+            nixfmt-rfc-style.enable = true;
+            nil.enable = true;
+            statix.enable = true;
+            flake-checker.enable = true;
+            deadnix.enable = true;
+            cargo-check.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+    };
+}
